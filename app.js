bea94415f59ce34f7b82adbfc1b4897ec31963e2
CodeAGenie <nwibeg@gmail.com>
2023-08-10 11:51:57 +0100

    Adding Javascript

---
+++ app.js
@@ -0,0 +1,41 @@
+// Homepage
+
+const getStarted = document.getElementById('getStarted').addEventListener('click',goToSignUpPage)
+
+const loginFromHeader = document.getElementById('loginFromHeader').addEventListener('click',goToLoginPage)
+
+const signUp = document.getElementById('signUpFromHeader').addEventListener('click',goToSignUpPage)
+
+// Signup
+
+const signUpEmail = document.getElementById('signUpEmail').addEventListener('input',validateEmailGiveFeedBack)
+
+const errorMessage = document.getElementById('email-error')
+
+const errorMessageIcon = document.getElementById('errorMessageIcon')
+
+const signUpPassword =  document.getElementById('signUpPassword').addEventListener('blur',validatePasswordGiveFeedBack)
+
+const confirmPassword = document.getElementById('confirmPassword').addEventListener('keyup',compareAndConfirmPassword)
+
+const finishedSigningUp =document.getElementById('finishedSigningUp').addEventListener('click',redirectToContentGenerator)
+
+const alreadyHaveAccount = document.getElementById('alreadyHaveAccount').addEventListener('click',goToLoginPage)
+
+// Login/sign in
+const loginEmail =document.getElementById('loginEmail').addEventListener('input',validateEmailGiveFeedBack)
+
+const loginPassword =document.getElementById('loginPassword').addEventListener('input',validatePasswordGiveFeedBack)
+
+const signInReturningUser =document.getElementById('signInReturningUser').addEventListener('click',goToLoginPage)
+
+const notRegisteredYet =document.getElementById('notRegisteredYet').addEventListener('click',goToSignUpPage)
+
+// Content generator
+
+
+
+
+
+
+
